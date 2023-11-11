f143ae642a98030c73cce9953402562d722a4fb9
Breadus1 <116367179+Breadus1@users.noreply.github.com>
2023-11-11 17:18:19 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,23 @@
+Autor Marcel Wątruch 2A
+Data 11.11.2023
+Przedmiot na który jest ściąga Historia
+
+
+
+Aby ponownie sprawdzić czy program działa 
+trzeba wcisnąć wycofaj
+
+
+
+Dołączone zostały dwa pliki skryptu .py oraz 
+.exe .py jest tylko po to by zobaczyć jak 
+działa program
+
+
+
+W folderze rezultat znajdują się pliki/foldery 
+które powinny pojawić się PO uruchomieniu 
+skryptu/programu. Nie są one wynikiem działania
+bezpośrednio programu tylko to co powinno się
+pojawić.
+
---
+++ bazadanych.sql
@@ -0,0 +1,48 @@
+CREATE TABLE IF NOT EXISTS tekst (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tresc TEXT
+);
+
+INSERT INTO tekst (tresc) VALUES ('Bohaterowie');
+INSERT INTO tekst (tresc) VALUES ('Zygmunt I Stary (1467-1548) - Król Polski od 1506, znany z wzmocnienia władzy monarszej i wprowadzenia reform prawnych.');
+INSERT INTO tekst (tresc) VALUES ('Zygmunt II August (1520-1572) - Ostatni Jagiellon na polskim tronie, jego panowanie to czas unii polsko-litewskiej.');
+INSERT INTO tekst (tresc) VALUES ('Mikołaj Kopernik (1473-1543) - Wielki astronom, autor teorii heliocentrycznej, rewolucjonizującej ówczesny obraz świata.');
+INSERT INTO tekst (tresc) VALUES ('Jan Kochanowski (1530-1584) - Wybitny poeta, autor fraszek, trenów i pieśni, odnowiciel polskiej literatury.');
+INSERT INTO tekst (tresc) VALUES ('Andrzej Frycz Modrzewski (1503-1572) - Humanista i reformator społeczny, autor dzieła O poprawie Rzeczypospolitej.');
+INSERT INTO tekst (tresc) VALUES ('Stanisław Hozjusz (1504-1579) - Duchowny katolicki, kardynał, przeciwnik reformacji.');
+INSERT INTO tekst (tresc) VALUES ('Jan Zamoyski (1542-1605) - Stanisław Hozjusz, działacz polityczny, kanclerz i hetman wielki koronny.');
+INSERT INTO tekst (tresc) VALUES ('Barbara Radziwiłłówna (1520-1551) - Królowa Polski, druga żona Zygmunta Augusta, jej postać wywołała konflikty na tle politycznym.');
+
+INSERT INTO tekst (tresc) VALUES ('Daty');
+INSERT INTO tekst (tresc) VALUES ('1506 - Zygmunt I Stary zostaje królem Polski.');
+INSERT INTO tekst (tresc) VALUES ('1525 - Hołd Pruski, zakon krzyżacki staje się lennikiem Polski.');
+INSERT INTO tekst (tresc) VALUES ('1543 - Publikacja "O obrotach sfer niebieskich" Kopernika.');
+INSERT INTO tekst (tresc) VALUES ('1569 - Unia Lubelska, formalne połączenie Polski i Litwy w Rzeczpospolitą Obojga Narodów.');
+INSERT INTO tekst (tresc) VALUES ('1572 - Śmierć Zygmunta Augusta i koniec dynastii Jagiellońskiej.');
+INSERT INTO tekst (tresc) VALUES ('1514 - Bitwa pod Orszą, zwycięstwo Polski nad Wielkim Księstwem Moskiewskim.');
+INSERT INTO tekst (tresc) VALUES ('1531 - Bitwa pod Obertynem, zwycięstwo nad Mołdawią, potwierdzenie polskich wpływów na Wołoszczyźnie.');
+INSERT INTO tekst (tresc) VALUES ('1558-1582 - Wojna o Inflanty, konflikt z Rosją, Szwecją i Danią.');
+INSERT INTO tekst (tresc) VALUES ('1573 - Pierwsza wolna elekcja, Henryk Walezy zostaje królem Polski.');
+INSERT INTO tekst (tresc) VALUES ('1596 - Unia brzeska, utworzenie Kościoła unickiego.');
+
+INSERT INTO tekst (tresc) VALUES ('Wydarzenia');
+INSERT INTO tekst (tresc) VALUES ('Reformacja i kontrreformacja - Ruchy religijne znacząco wpływające na kulturę i politykę.');
+INSERT INTO tekst (tresc) VALUES ('Rozwój demokracji szlacheckiej - Sejm i wolna elekcja, unikalne w skali europejskiej.');
+INSERT INTO tekst (tresc) VALUES ('Rozkwit kultury i nauki - Oświecenie polskie, rozwój drukarstwa, sztuki i nauki.');
+INSERT INTO tekst (tresc) VALUES ('Rozbicie Dzielnicowe - Proces konsolidacji ziem polskich po okresie rozbicia.');
+INSERT INTO tekst (tresc) VALUES ('Powstanie Duchowieństwa Protestanckiego - Rozwój różnych odłamów protestantyzmu w Polsce.');
+INSERT INTO tekst (tresc) VALUES ('Konflikty zewnętrzne - Wojny z Zakonem Krzyżackim, Moskwą, Turcją i Tatarami.');
+INSERT INTO tekst (tresc) VALUES ('Rozwój Parlamentaryzmu - Ukształtowanie się dwuizbowego sejmu (Senat i Izba Poselska).');
+
+INSERT INTO tekst (tresc) VALUES ('Pojęcia');
+INSERT INTO tekst (tresc) VALUES ('Złoty Wiek Polski - Okres prosperity, rozwoju kulturalnego i politycznego.');
+INSERT INTO tekst (tresc) VALUES ('Sytuacja społeczna - Dominacja szlachty, kwestia chłopów i rozwijającego się mieszczaństwa.');
+INSERT INTO tekst (tresc) VALUES ('Stosunki międzynarodowe - Polska jako znaczący gracz w Europie, relacje z sąsiednimi państwami.');
+INSERT INTO tekst (tresc) VALUES ('Wpływ na Europę - Polska jako ważny ośrodek kulturowy i naukowy, wpływający na kierunki rozwoju europejskiego.');
+INSERT INTO tekst (tresc) VALUES ('Unia Lubelska (1569) - Umowa między Królestwem Polskim a Wielkim Księstwem Litewskim, tworząca federację obu państw.');
+INSERT INTO tekst (tresc) VALUES ('Wolna Elekcja - Unikalny w Europie system wyboru króla przez szlachtę bez udziału dziedziczności.');
+INSERT INTO tekst (tresc) VALUES ('Nihil novi (1505) - Ustawa ograniczająca władzę królewską, uznawana za początek "demokracji szlacheckiej".');
+INSERT INTO tekst (tresc) VALUES ('Sarmatyzm - Ideologia i styl życia polskiej szlachty, oparty na mitologizacji i wyidealizowanym obrazie starożytnych Sarmatów.');
+
+INSERT INTO tekst (tresc) VALUES ('Autor: Marcel Wątruch 2a');
+
